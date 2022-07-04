c15816749cb9fde9dda813adde669ff412de2a1a
Dinis <d.yudenka@yandex.by>
2022-07-04 22:27:16 +0300

    refactor: add API key

--- migrationToTS/src/components/controller/appLoader.js
+++ migrationToTS/src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: '6943074f3b9a4cf1a50127f212238b1a',
         });
     }
 }
