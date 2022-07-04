90a7d4a9aa10cefb0f92804914eccd7375736b9f
Dinis <d.yudenka@yandex.by>
2022-07-05 01:37:58 +0300

    feat: migrate to typescript in the news file

--- migrationToTS/src/types/index.ts
+++ migrationToTS/src/types/index.ts
@@ -1,20 +1,31 @@
 export interface INewsResponse {
-    status: string,
-    totalResults: number,
-    articles: Article[]
+    status: string;
+    totalResults: number;
+    articles: Article[];
 }
 
-type Article = {
-    author: string | null,
-    content: string,
-    description: string,
-    publishedAt: string,
-    sources: {
-        id: string,
-        name: string
-    },
-    title: string,
-    url: string,
-    urlToImage: string
+export type Article = {
+    author: string | null;
+    content: string;
+    description: string;
+    publishedAt: string;
+    source: ISource;
+    title: string;
+    url: string;
+    urlToImage: string;
 }
+
+export interface ISource {
+    id: string;
+    name: string;
+    description?: string;
+    url?: string;
+    category?: string;
+    language?: string;
+    country?: string;
+}
+
+export interface IDraw<T> {
+    draw: (data: T[]) => void;
+} 
 
