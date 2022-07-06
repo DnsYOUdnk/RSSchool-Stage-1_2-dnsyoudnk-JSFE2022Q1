f493f79c35c044ff6a8837ad8ea2b5bf1a0c08e0
Dinis <d.yudenka@yandex.by>
2022-07-06 13:19:35 +0300

    fix: change the url to work in gh-pages

--- migrationToTS/src/components/controller/appLoader.ts
+++ migrationToTS/src/components/controller/appLoader.ts
@@ -2,7 +2,7 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
     constructor() {
-        super('https://newsapi.org/v2/', {
+        super('https://nodenews.herokuapp.com/', {
             apiKey: '6943074f3b9a4cf1a50127f212238b1a',
         });
     }
