f26be3f9a0521996f41bdd889651f0aca4323816
Dinis <d.yudenka@yandex.by>
2022-07-18 22:53:35 +0300

    feat: add the number of products to cart

--- online-store/src/components/Header/Header.tsx
+++ online-store/src/components/Header/Header.tsx
@@ -1,16 +1,28 @@
+import { useContext } from 'react';
 import { Link } from 'react-router-dom';
-import cart from './../../assets/svg/cart.svg'
+import { Context } from '../../StoreContext';
+import cartSVG from './../../assets/svg/cart.svg'
 import './Header.css'
 
 export const Header = function() {
+  const { cart } = useContext(Context);
+
+  const getCartCount = () => {
+    if(cart.length === 0) {
+      return ''
+    } else {
+      return cart.length
+    }
+  }
+
   return (
   <header className="header">
     <div className="header__logo">
       <Link to='/'>Online Store</Link>
     </div>
     <div className="header__widget">
-      <input type='text'id='header__widget__value' placeholder='Search'/>
-      <Link to='/'><img src={cart} alt="cart"/></Link>
+      <input type="text" id="header__widget__value" placeholder="Search"/>
+      <Link to="/" className="header__cart"><img src={cartSVG} alt="cart"/><span className="header__cart_count">{getCartCount()}</span></Link>
     </div>
   </header>
 )}
