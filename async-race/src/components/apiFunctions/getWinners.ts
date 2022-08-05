0aa05d594cf46ce6161f2442d3898f13c9bf46d2
Dinis <d.yudenka@yandex.by>
2022-08-05 12:20:54 +0300

    feat: implement the function get winners

---
+++ async-race/src/components/apiFunctions/getWinners.ts
@@ -0,0 +1,20 @@
+import { ParamWinners, IWinners } from '../../types';
+import { winners } from './api';
+import { getCar } from './getCar';
+
+export const getWinners = async ({
+  page, limit = 10, sort, order,
+}: ParamWinners): Promise<{ items: IWinners[], count: number }> => {
+  const sortOrder = sort && order ? `&_sort=${sort}&_order=${order}` : '';
+  const res = await fetch(`${winners}?_page=${page}&_limit=${limit}${sortOrder}`);
+  const arrWinners = await res.json();
+  const totalCount = Number(res.headers.get('X-Total-Count'));
+  const items = await Promise.all(arrWinners.map(async (winner: { id: number }) => (
+    { ...winner, car: await getCar(winner.id) }
+  )));
+
+  return {
+    items,
+    count: totalCount,
+  };
+};
--- async-race/src/types/index.ts
+++ async-race/src/types/index.ts
@@ -1,10 +1,24 @@
 export interface Car {
-  name: string;
-  color: string;
-  id?: number;
+  name: string,
+  color: string,
+  id?: number,
 }
 
 export interface DataMotion {
-  velocity: number;
-  distance: number;
+  velocity: number,
+  distance: number,
+}
+
+export interface ParamWinners {
+  page: number,
+  limit: number
+  sort: string,
+  order: string,
+}
+
+export interface IWinners {
+  id: number,
+  wins: number,
+  time: number,
+  car: Car,
 }
