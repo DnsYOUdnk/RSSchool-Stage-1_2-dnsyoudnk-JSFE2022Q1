7708f8c69590b47ba8aa30cdd9a6cfcd5b5e60a8
Dinis <d.yudenka@yandex.by>
2022-08-05 11:54:19 +0300

    feat: implement the function of drive car

--- async-race/src/components/apiFunctions/changeEngineCar.ts
+++ async-race/src/components/apiFunctions/changeEngineCar.ts
@@ -3,14 +3,22 @@ import { engine } from './api';
 
 export const startEngine = async (id: number): Promise<DataMotion> => {
   const res = await fetch(`${engine}?id=${id}&status=started`, {
-    method: 'PATH',
+    method: 'PATCH',
   });
   return res.json();
 };
 
 export const stopEngine = async (id: number): Promise<DataMotion> => {
   const res = await fetch(`${engine}?id=${id}&status=stopped`, {
-    method: 'PATH',
+    method: 'PATCH',
   });
   return res.json();
+};
+
+export const driveCar = async (id: number): Promise<{ success: boolean }> => {
+  const res = await fetch(`${engine}?id=${id}&status=drive`, {
+    method: 'PATCH',
+  }).catch();
+  const { status } = res;
+  return status === 200 ? { ...(await res.json()) } : { success: false };
 };
